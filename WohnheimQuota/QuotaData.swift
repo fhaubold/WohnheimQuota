ba988cf80a7d261480cdb82f5907f45b7698323c
Florian Haubold <florian@buntes116.wohnheim.uni-kl.de>
2015-03-25 21:21:00 +0100

    First progress bar, changed one value to double

--- WohnheimQuota/QuotaData.swift
+++ WohnheimQuota/QuotaData.swift
@@ -8,6 +8,67 @@
 
 import Foundation
 
-struct QuotaData {
-    //let
+class QuotaData {
+    // Basic properties
+    let url = NSURL(string: "https://quota.wohnheim.uni-kl.de")
+    var download: String = ""
+    var upload: String = ""
+    var downPercents: Double = 0
+    var upPercents: Int = 0
+    var timePercents: Int = 0
+    
+    // constructor
+    init() {
+        loadData()
+    }
+    
+    // function for loading data from Quota Website
+    func loadData() {
+        var html = NSString(contentsOfURL: url!, encoding: NSUTF8StringEncoding, error: nil)!
+        
+        html = html.stringByReplacingOccurrencesOfString("<!--Wohnheime_incoming-->", withString: "")
+        html = html.stringByReplacingOccurrencesOfString("<!--Wohnheime_outgoing-->", withString: "")
+        html = html.stringByReplacingOccurrencesOfString(" <!--end-->", withString: "")
+        
+        var err : NSError?
+        var parser = HTMLParser(html: html, error: &err)
+        if err != nil {
+            println(err)
+            exit(1)
+        }
+        
+        var bodyNode = parser.body
+        
+        var results = NSMutableArray()
+        var percents = NSMutableArray()
+        
+        if let inputNodes = bodyNode?.findChildTags("td") {
+            for node in inputNodes {
+                results.addObject(node.contents)
+                if (node.getAttributeNamed("title") as NSString != "") {
+                    percents.addObject(node.getAttributeNamed("title"))
+                }
+            }
+        }
+        
+        let indexSet = NSMutableIndexSet()
+        for var j=0; j<6; j++ {
+            indexSet.addIndex(j)
+        }
+        indexSet.addIndex(7)
+        indexSet.addIndex(8)
+        
+        results.removeObjectsAtIndexes(indexSet)
+        
+        download = results[0] as String
+        upload = results[1] as String
+        
+        for percent in percents {
+            (percent as String).stringByReplacingOccurrencesOfString("%", withString: "")
+        }
+        
+        downPercents = (percents[0] as NSString).doubleValue
+        upPercents = (percents[1] as String).toInt()!
+        timePercents = (percents[2] as String).toInt()!
+    }
 }
